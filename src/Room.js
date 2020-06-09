1b001e6ff4ac88316626390da4193361277764fc
syedadeeliqbal <syedadeeliqbal@gmail.com>
2020-06-09 14:03:36 -0400

    Commenting un-used functions

--- src/Room.js
+++ src/Room.js
@@ -7,14 +7,14 @@ function Room() {
     let [isLit, setLit] = useState(true);
     let [age, setAge] = useState(23);
 
-    function updateLit(){
-        setLit(!isLit);        
-        console.log("Button Clicked");
-    }
-    function updateAge(){        
-        setAge(++age);
-        console.log("Button Clicked");
-    }
+    // function updateLit(){
+    //     setLit(!isLit);        
+    //     console.log("Button Clicked");
+    // }
+    // function updateAge(){        
+    //     setAge(++age);
+    //     console.log("Button Clicked 1");
+    // }
   return (
     <div>
       This is Room component: Lit = {isLit ? "lit" : "Dark"}
