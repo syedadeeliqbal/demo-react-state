90d2a1c40769414175b769006d99218cfd8b3241
syedadeeliqbal <syedadeeliqbal@gmail.com>
2020-06-13 11:37:01 -0400

    Adding temperature and on off buttons

--- src/Room.js
+++ src/Room.js
@@ -7,6 +7,7 @@ function Room() {
     // console.log(state);
     let [isLit, setLit] = useState(true);
     let [age, setAge] = useState(23);
+    let [temperature, setTemperature] = useState(22);
 
     // function updateLit(){
     //     setLit(!isLit);        
@@ -21,12 +22,20 @@ function Room() {
     // <div className={"room " + (isLit ? "lit" : "dark")}>
     <div className={`room ${isLit ? "lit" : "dark"}`}>
       This is Room component: Lit = {isLit ? "lit" : "Dark"}
+      This current temperature is: {temperature}
       <br/>
       My age is: {age}
       <br/>
       <button onClick={() => setLit(!isLit)} >Toggle Light</button>
       <br/>
       <button onClick={() => setAge(++age)} >Toggle Age</button>
+
+      <br/>
+      <button onClick={() => setLit(true)} >On</button>      
+      <button onClick={() => setLit(false)} >Off</button>
+      <br/>
+      <button onClick={() => setTemperature(++temperature)} >+</button>      
+      <button onClick={() => setTemperature(--temperature)} >-</button>
 
     </div>
   );
